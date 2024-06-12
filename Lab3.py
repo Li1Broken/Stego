416262d40238db96a509b2c6797543bf5b041585
Li1Broken <88087517+Li1Broken@users.noreply.github.com>
2024-06-12 22:38:58 +0300

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -2,37 +2,29 @@ def text_to_binary(text):
     binary_string = ""
     for char in text:
         binary_char = bin(ord(char))[2:]
-        binary_char = binary_char.zfill(8)
+        binary_char = binary_char.zfill(6)
         binary_string += binary_char
     return binary_string
-
-def count_ones(binary_string):
-    return binary_string.count('1')
-
 
 
 filename = "input.txt"
 output_filename = "output.txt"
-total_ones = 0
 
 with open(filename, 'r', encoding='utf-8') as file, open(output_filename, 'w', encoding='utf-8') as output_file:
     for line in file:
         line = line.rstrip()
         number = text_to_binary(line)
-        ones_count = count_ones(number)
-        total_ones += ones_count
+        has_six_zeros = '000000' in number
         output_file.write(f"Строка:\n {line}\n")
         output_file.write(f"Двоичное представление: {number}\n")
-        output_file.write(f"Сумма единиц: {ones_count}\n")
-        if ones_count % 2 == 0: 
-            output_file.write('Сумма единиц четная -> Y\n\n')
-        else: 
-            output_file.write('Сумма единиц нечетная -> N\n\n')
+        if has_six_zeros:
+            output_file.write('Нет - в строке встречается шесть нулей подряд\n\n')
+        else:
+            output_file.write('Да - в строке не встречается шесть нулей подряд\n\n')
         # Также выводим на консоль
         print(f"Строка:\n {line}")
         print(f"Двоичное представление: {number}")
-        print(f"Сумма единиц: {ones_count}")
-        if ones_count % 2 == 0: 
-            print('Сумма единиц четная -> Y\n')
-        else: 
-            print('Сумма единиц нечетная -> N\n')
+        if has_six_zeros:
+            print('Нет - в строке встречается шесть нулей подряд\n')
+        else:
+            print('Да - в строке не встречается шесть нулей подряд\n')
